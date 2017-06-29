562cda62420b680f78429eb3583681f4890698e8
Thalia <thalia.infante@outlook.com>
2017-06-29 00:59:08 +0000

    Adds imageuploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315075429) do
+ActiveRecord::Schema.define(version: 20170624171349) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170315075429) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
 end
